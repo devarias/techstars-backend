82bb9a42e3c2d12063142178a40bf3f23c016f45
devarias <gedafu2005@gmail.com>
2021-03-10 21:10:38 -0500

    Stablished info about the DB

--- src/database/database.js
+++ src/database/database.js
@@ -1,24 +1,29 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres', 'postgres', 'postgres', {
-  host: 'localhost',
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false, // <<<<<<< THIS
+const sequelize = new Sequelize(
+  'dbt3gie7ucbio0',
+  'ozyzmwnbqlktme',
+  'a2bf6dc5eeefb001457d353c87c48dba3747be223ba65baa125a2e943e2428a9',
+  {
+    host: 'ec2-54-164-22-242.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< THIS
+      },
     },
-  },
-  pool: {
-    max: 20,
-    min: 0,
-    require: 30000,
-    idle: 10000,
-  },
-  logging: false,
-  define: {
-    freezeTableName: true,
-    timestamps: false,
-  },
-});
+    pool: {
+      max: 20,
+      min: 0,
+      require: 30000,
+      idle: 10000,
+    },
+    logging: false,
+    define: {
+      freezeTableName: true,
+      timestamps: false,
+    },
+  }
+);
 module.exports = sequelize;
